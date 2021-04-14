93268bf9cb8c24fd3504b5cc2d2b0e7b2c884523
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-04-14 02:51:33 -0400

    Add Selectable Skins System

--- Assets/GameEssentials/Scripts/Clases/SkinPlayer.cs
+++ Assets/GameEssentials/Scripts/Clases/SkinPlayer.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class SkinPlayer {
     public int id;
     public float cost;
-    public bool purchased;
+    public bool purchased, tintable, colorRandom;
+    public Color colorSelect;
     public Sprite skin;
     public List<AudioClip> winClips, backgroundMusics;
+
+    public SkinPlayer(int _id){
+        id = _id;
+        cost = 0f;
+        purchased = tintable = false;
+        colorSelect = Color.white;
+        skin = null;
+        winClips = new List<AudioClip>();
+        backgroundMusics = new List<AudioClip>();
+    }
 }
--- Assets/GameEssentials/Scripts/Clases/SkinsDataBase.cs
+++ Assets/GameEssentials/Scripts/Clases/SkinsDataBase.cs
@@ -1,17 +1,12 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Databases/Skins", fileName = "SkinsDataBase")]
 public class SkinsDataBase : ScriptableObject{
 
     public List<SkinPlayer> skins = new List<SkinPlayer>();
 
-    public List<SkinPlayer> GetPurchasedkins(){
-        List<SkinPlayer> tmpList = new List<SkinPlayer>();
-        foreach (var skin in skins)
-            if(skin.purchased)
-                tmpList.Add(skin);
-        return tmpList;
-    }
+    public List<SkinPlayer> GetPurchasedkins() => skins.FindAll(s => s.purchased);
 
+    public SkinPlayer GetByID(int id) => skins.Find(s => s.id == id);
 }
